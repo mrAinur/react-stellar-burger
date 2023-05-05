8fd9fbfba1ab59815d2ebd1f7e2f0f9fd0719c31
Ainur <mr.ainur.02@mail.ru>
2023-05-05 22:16:41 +0500

    Не работающий API

--- src/components/GetBurger/GetBurger.jsx
+++ src/components/GetBurger/GetBurger.jsx
@@ -2,13 +2,19 @@ import style from './GetBurger.module.css'
 import BurgerConstructor from '../BurgerConstructor/BurgerConstructor';
 import BurgerIngredients from '../BurgerIngredients/BurgerIngredients';
 import { data } from '../../utils/data';
+import { ingredientPropType } from '../../utils/prop-types';
+import PropTypes from "prop-types";
 
 
-export default function GetBurger() {
+export default function GetBurger(props) {
   return (
-        <section className={style.GetBurgerContent}>
-          <BurgerConstructor data={data} />
-          <BurgerIngredients data={data}/>
-        </section>
+    <section className={style.GetBurgerContent}>
+      <BurgerConstructor data={data} />
+      <BurgerIngredients data={data} />
+    </section>
   )
 }
+
+BurgerConstructor.propTypes = {
+  data: PropTypes.arrayOf(ingredientPropType.isRequired).isRequired
+};
--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,12 +1,49 @@
+import React from "react";
 import Header from "../AppHeader/Header";
 import GetBurger from "../GetBurger/GetBurger";
 
 function App() {
+  const getIngredientsInfo = "https://norma.nomoreparties.space/api/ingredients";
+
+  const [state, setState] = React.useState();
+
+  React.useEffect(() => {
+    // const getIngredientsData = async () => {
+    //   setState({ ...state, isLloading: true });
+    //   const res = await fetch(getIngredientsInfo);
+    //   if (res.ok) {
+    //   } else {
+    //     return Promise.reject(`Ошибка ${res.status}`);
+    //   }
+    //   const data = await res.json();
+    //   setState({...state, isLoading: false, ingredientsData: data.data})
+    // }
+    // getIngredientsData();
+    fetch(getIngredientsInfo)
+      .then((res) => {
+        if (res.ok) {
+          return res = res.json();
+        } else {
+          return Promise.reject(`Ошибка ${res.status}`);
+        }
+      })
+      .then((res) => {
+        setState(
+          res.data
+        )
+      })
+      .catch((rej) => {
+        console.log(`Ошибка ${rej.status}`);
+      })
+  }, []);
+
+  console.log(state)
+
   return (
     <>
       <Header />
       <main>
-        <GetBurger />
+        <GetBurger data={state} />
       </main>
       <footer className="mt-10"></footer>
     </>
